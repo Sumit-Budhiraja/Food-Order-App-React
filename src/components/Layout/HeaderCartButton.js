d66fedb61bf630fd62c12784299e0cdd1f15430a
Sumit Budhiraja <48406445+Sumit-Budhiraja@users.noreply.github.com>
2021-06-08 21:20:10 +0530

    Added functionality for CartItems

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,35 @@
-import React, { useContext } from "react";
+import React, { useContext,useEffect,useState } from "react";
 import classes from "./HeaderCartButton.module.css";
 import CardIcon from "../Cart/CartIcon";
-import {useState} from 'react';
+//import {useState} from 'react';
 import CartContext from '../../store/cart-context'
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted,setBtnIsHighlighted] = useState(false);
   const cartCtx=useContext(CartContext);//this component will automatically be updated by react whenever the context is changed  
   const numberOfCartItems=cartCtx.items.reduce((curNumber,item)=>{
     return curNumber+item.amount;
   },0);
+
+  const {items}=cartCtx;
+  const btnClasses= `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+  useEffect(()=>{
+    if(items.length===0){
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer=setTimeout(()=>{//so that the bump class is removed after every 300 mSec
+      setBtnIsHighlighted(false);
+    },300);
+
+    return ()=>{
+      clearTimeout(timer);
+    }
+  },[items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CardIcon />
       </span>
